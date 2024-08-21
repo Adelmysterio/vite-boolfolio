207981cb91ff40ccede02bcc2271e7120557adb4
AdelMysterio <storliniadelmo@gmail.com>
2024-08-21 16:54:30 +0200

    creato componente appmain e effettuata chimata per ricevere dati

---
+++ src/components/AppMain.vue
@@ -0,0 +1,42 @@
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            projects: [],
+        }
+    },
+    methods: {
+        getProjects() {
+            axios.get('http://127.0.0.1:8000/api/projects', {
+                params: {
+                }
+            })
+                .then((response) => {
+                    console.log(response.data.response);
+                    this.projects = response.data.response;
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+                .finally(function () {
+                    // always executed
+                });
+        }
+    },
+    created() {
+        this.getProjects();
+    },
+}
+</script>
+
+<template>
+    <ul>
+        <li v-for="(project, index) in projects" :key="index">
+            {{ project.name }}
+        </li>
+    </ul>
+
+</template>
+
+<style lang="scss" scoped></style>
